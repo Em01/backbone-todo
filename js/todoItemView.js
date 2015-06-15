32acfc09ae4a66af9f1392442c71e6a5f515ec41
Em01 <emma_williams1@hotmail.co.uk>
2015-06-15 12:01:28 +0100

    Basic state for checkboxes

--- js/todoItemView.js
+++ js/todoItemView.js
@@ -6,8 +6,16 @@ var TodoItemView = Backbone.View.extend({
 			throw new Error("model is not specified.");
 	},
 
+	events: {
+		"click #toggle": "onClickToggle"
+	},
+
+	onClickToggle: function(){
+		console.log("Toggle clicked");
+	},
+
 	render: function(){
-		this.$el.html(this.model.escape("description"));
+		this.$el.html("<input id='toggle' type='checkbox'></input>" + this.model.escape("description"));
 
 		return this;
 	}
